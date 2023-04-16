f230e045476b1734b04ff66c3cf66518d50e1192
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2023-04-16 15:50:11 +0700

    Add chart

--- src/components/DashboardView.vue
+++ src/components/DashboardView.vue
@@ -2,13 +2,13 @@
     <div>
         <navbar-component></navbar-component>
         <sidebar-component></sidebar-component>
-        <div class="content-wrapper">            
+        <div class="content-wrapper">
             <div class="content-header">
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
                             <h1 class="m-0">Dashboard</h1>
-                        </div>          
+                        </div>
                     </div>
                 </div>
             </div>
@@ -22,29 +22,67 @@
                                     <h5 class="card-title">Selamat Datang</h5>
 
                                     <p class="card-text">
-                                        Website Sistem Informasi Perpustakaan   
+                                        Website Sistem Informasi Perpustakaan
                                     </p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <div style="width: 300px; height: 300px;">
+                    <canvas id="piechart" width="300" height="300"></canvas>
+                </div>
             </div>
-        </div>        
+        </div>
     </div>
 </template>
 
 <script>
 
-    import NavigationBar from '../components/template/NavigationBar.vue'
-    import AppSidebar from '../components/template/AppSidebar.vue'
-    
+import NavigationBar from '../components/template/NavigationBar.vue'
+import AppSidebar from '../components/template/AppSidebar.vue'
+
+import axios from 'axios'
+import Chart from 'chart.js/auto'
 
 export default {
     components: {
-        'navbar-component' : NavigationBar,
-        'sidebar-component' : AppSidebar,
+        'navbar-component': NavigationBar,
+        'sidebar-component': AppSidebar,
+    },
+    data() {
+        return {
+            databuku: {},
+        }
+    },
+    mounted() {
+        axios.get('http://localhost:8000/api/getbuku')
+            .then(
+                (response) => {
+                    const data = response.data.map(item => item.jumlah_pinjam)
+                    const nama = response.data.map(item => item.judul_buku)
 
-  }
+                    const chartData = {
+                        labels: nama,
+                        datasets: [{
+                            data: data,
+                            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0']
+                        }]
+                    }
+
+                    new Chart(document.getElementById('piechart'), {
+                        type: 'bar',
+                        data: chartData,
+                        options: {
+                            responsive: true
+                        }
+                    })
+                })
+            .catch(
+                err => {
+                    console.log(err)
+                }
+            )
+    }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import UploadForm from './components/BukuView.vue'
+// import UploadForm from './components/BukuView.vue'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 Vue.config.productionTip = false
